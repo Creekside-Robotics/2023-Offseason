40badad7cd27369c53c485989d8525f0fbe0b855
josephhadad05 <91989021+josephhadad05@users.noreply.github.com>
2023-05-13 12:46:51 -0400

    Adding SetPose Command

---
+++ src/main/java/frc/robot/commands/drivetrain/SetPose.java
@@ -0,0 +1,46 @@
+// Copyright (c) FIRST and other WPILib contributors.
+// Open Source Software; you can modify and/or share it under the terms of
+// the WPILib BSD license file in the root directory of this project.
+
+package frc.robot.commands.drivetrain;
+
+import java.util.function.Supplier;
+
+import edu.wpi.first.math.geometry.Pose2d;
+import edu.wpi.first.wpilibj2.command.InstantCommand;
+import frc.robot.subsystems.Drivetrain;
+
+// NOTE:  Consider using this command inline, rather than writing a subclass.  For more
+// information, see:
+// https://docs.wpilib.org/en/stable/docs/software/commandbased/convenience-features.html
+public class SetPose extends InstantCommand {
+  private Drivetrain drivetrain;
+  private Supplier<Pose2d> poseSupplier;
+
+  /**
+   * Creates a new SetPose command which sets the pose of the drivetrain.
+   * @param drivetrain Drivetrain object.
+   * @param pose Pose to set the drivetrain to.
+   */
+  public SetPose(Drivetrain drivetrain, Pose2d pose) {
+    // Use addRequirements() here to declare subsystem dependencies.
+    this.drivetrain = drivetrain;
+    this.poseSupplier = () -> pose;
+  }
+
+  /**
+   * Creates a new SetPose command which sets the pose of the drivetrain.
+   * @param drivetrain Drivetrain object.
+   * @param poseSupplier Suppier object, which provides a pose to set the drivetrain to.
+   */
+  public SetPose(Drivetrain drivetrain, Supplier<Pose2d> poseSupplier){
+    this.drivetrain = drivetrain;
+    this.poseSupplier = poseSupplier;
+  }
+
+  // Called when the command is initially scheduled.
+  @Override
+  public void initialize() {
+    this.drivetrain.setDrivetrainPose(this.poseSupplier.get());
+  }
+}
